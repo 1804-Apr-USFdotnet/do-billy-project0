5954a98229336ce8dc6449b308b267e6e5588184
Billy Do <dob@utexas.edu>
2018-04-23 10:50:05 -0400

    adding base library

---
+++ PalindromeCheck/PalindromeCheck/Palindromes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheck
+{
+    static public class Palindromes
+    {
+        static bool IsPalindrome(string str)
+        {
+            string reversed;
+            try
+            {
+                reversed = str.Reverse().ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                return false;
+            }
+            return str == reversed;
+        }
+    }
+}
---
+++ PalindromeCheck/PalindromesUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PalindromesUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("PalindromesUnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("204492e7-e134-474c-96d6-b9d6dae481f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
