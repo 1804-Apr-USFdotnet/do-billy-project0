5199898b8591acb5953b9be6c25f1232c82fbbd6
Billy Do <dob@utexas.edu>
2018-04-27 13:50:21 -0400

    added changed filenames

---
+++ RestaurantReviewsLibrary/RestaurantReviewsLibrary/Models/RestaurantInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestaurantReviewsLibrary.Abstracts;
+using RestaurantReviewsLibrary.Interfaces;
+
+namespace RestaurantReviewsLibrary.Models
+{
+    public class RestaurantInfo : Business, IRestaurantInfo
+    {
+        private List<IReview> ListOfReviews;
+
+        public double AverageRating
+        {
+            get
+            {
+                if (!(ListOfReviews.Count > 0))
+                {
+                    return 0;
+                }
+                double sum = 0.0;
+                foreach(IReview review in ListOfReviews)
+                {
+                    sum += review.Rating;
+                }
+                return sum / ListOfReviews.Count;
+            }
+        }
+
+        public RestaurantInfo(string name, string loc) : base(name, loc)
+        {
+        }
+
+        public void SubmitReview(int rating, string name, string description = "")
+        {
+            ListOfReviews.Add(new Review(rating, name, DateTime.Now, description));
+        }
+
+        public List<IReview> GetAllReviews()
+        {
+            // copy of ListOfReviews?
+            // ListOfReviews is private
+            return ListOfReviews;
+        }
+
+
+    }
+}
