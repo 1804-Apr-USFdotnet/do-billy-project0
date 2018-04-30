34c06c293ff443e1f00e8aff3a4443f94d27b636
Billy Do <dob@utexas.edu>
2018-04-30 15:16:26 -0400

    implemented remaining tests

--- RestaurantReviewsLibrary/RRLibraryUnitTest/Models/RestaurantsInfoUnitTests.cs
+++ RestaurantReviewsLibrary/RRLibraryUnitTest/Models/RestaurantsInfoUnitTests.cs
@@ -25,58 +25,78 @@ namespace RestaurantReviewsLibrary.Models.Tests
             Assert.AreEqual(expectedReviewCount, actualReviewCount);
         }
 
-        [TestMethod()]
-        public void GetTopAverageRatingsTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void GetSerializedDataTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void GetDataTest()
-        {
-            Assert.Fail();
-        }
 
         [TestMethod()]
         public void GetTopRestaurantsTest()
         {
-            Assert.Fail();
+            // Dummy Data: Franklin BBQ, Pizza Place (Tallahassee), Checkers
+            RestaurantsInfo infoObj = new RestaurantsInfo();
+            string first = "Franklin BBQ";
+            string second = "Pizza Place";
+            string secondLocation = "Tallahassee";
+            string third = "Checkers";
+
+            var a = infoObj.GetTopRestaurants(3);
+
+            Assert.AreEqual(first, a.ElementAt(0).Name);
+            Assert.AreEqual(second, a.ElementAt(1).Name);
+            Assert.AreEqual(secondLocation, a.ElementAt(1).Location);
+            Assert.AreEqual(third, a.ElementAt(2).Name);
+
         }
 
         [TestMethod()]
         public void GetAllRestaurantsTest()
         {
-            Assert.Fail();
-        }
+            RestaurantsInfo infoObj = new RestaurantsInfo();
+            int expectedRestaurantCount = 7;
 
-        [TestMethod()]
-        public void GetRestaurantDetailTest()
-        {
-            Assert.Fail();
-        }
+            var a = infoObj.GetAllRestaurants();
 
-        [TestMethod()]
-        public void GetRestaurantDetailTest1()
-        {
-            Assert.Fail();
+            Assert.AreEqual(expectedRestaurantCount, a.Count());
+            foreach (var restaurant in a)
+            {
+                Assert.IsNotNull(restaurant.Name);
+            }
         }
 
         [TestMethod()]
         public void GetAllReviewsTest()
         {
-            Assert.Fail();
+            RestaurantsInfo infoObj = new RestaurantsInfo();
+            Dummy d = new Dummy();
+            int expectedReviewCount = d.GetReviewData().Count();
+
+            var actualReviews = infoObj.GetAllReviews();
+
+            Assert.AreEqual(expectedReviewCount, actualReviews.Count());
         }
 
         [TestMethod()]
-        public void SearchRestaurantTest()
+        public void SearchRestaurantTest1()
         {
-            Assert.Fail();
+            RestaurantsInfo infoObj = new RestaurantsInfo();
+            string expectedRestName = "Franklin BBQ";
+            int expectedCount = 1;
+
+            var a = infoObj.SearchRestaurant("Franklin BBQ");
+
+            Assert.AreEqual(expectedCount, a.Count());
+            Assert.AreEqual(expectedRestName, a.ElementAt(0).Name);
+        }
+
+        [TestMethod()]
+        public void SearchRestaurantTest2()
+        {
+            string restPartialName = "chuck e";
+            RestaurantsInfo infoObj = new RestaurantsInfo();
+            string expectedRestName = "Chuck E. Cheese";
+            int expectedCount = 1;
+
+            var a = infoObj.SearchRestaurant(restPartialName);
+
+            Assert.AreEqual(expectedCount, a.Count());
+            Assert.AreEqual(expectedRestName, a.ElementAt(0).Name);
         }
     }
 }
