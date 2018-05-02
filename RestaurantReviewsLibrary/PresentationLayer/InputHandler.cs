25dd8e10d156495b60da8e97df2f2953221ea875
Billy Do <dob@utexas.edu>
2018-05-01 22:32:22 -0400

    working on console

---
+++ RestaurantReviewsLibrary/PresentationLayer/InputHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer
+{
+    internal class InputHandler
+    {
+        public enum InputCommand { Unknown, Show, Details, Find};
+        public enum ShowCommand { Top, All }
+        static List<string> allowedCommands = new List<string> { "", "show", "details", "find" };
+
+        public static InputCommand DetermineCommand(string input)
+        {
+            // TODO: Implement
+            input = SanitizeInput(input);
+
+            var sa = input.Split();
+            if (sa.Count() > 0)
+            {
+                string maincommand = sa[0];
+                Console.WriteLine(maincommand);
+                InputCommand n = (InputCommand)allowedCommands.IndexOf(maincommand);
+                return n;
+            }
+            
+            return InputCommand.Unknown;
+        }
+
+        private static string SanitizeInput(string input)
+        {
+            return input.ToLower().Trim();
+        }
+    }
+}
--- RestaurantReviewsLibrary/PresentationLayer/Program.cs
+++ RestaurantReviewsLibrary/PresentationLayer/Program.cs
@@ -10,6 +10,34 @@ namespace PresentationLayer
     {
         static void Main(string[] args)
         {
+            string inputString = "";
+
+            while (inputString != "quit")
+            {
+                PrintCommands();
+
+                inputString = Console.ReadLine();
+                Console.Clear();
+
+                InputHandler.DetermineCommand(inputString);
+                //Console.WriteLine();
+            }
+        }
+
+        static void PrintCommands()
+        {
+            string commandstr = 
+                "\t\tCommands:" + "\n" +
+                "\t\tshow top" + "\n" +
+                "\t\tshow top N" + "\n" +
+                "\t\tshow all" + "\n" +
+                "\t\tshow [restaurant name]" + "\n" +
+                "\t\tdetails" + "\n" +
+                "\t\tfind" + "\n" +
+                "\t\tfind [restaurant]" + "\n" +
+                "\t\tquit\n";
+            Console.WriteLine(commandstr);
+            Console.Write("input >>> ");
         }
     }
 }
